2aa5eb157dc23db40539a685570d33463336bbe8
jango-fx <felix.groll@gmail.com>
2021-08-18 01:31:17 +0200

    added Demo

---
+++ Demo/SerialReceiveDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ƒx.UnityUtils.Serial;
+
+public class SerialReceiveDemo : MonoBehaviour
+{
+    public SimpleSerialManager serialManager;
+    public int messageCount = 0;
+    public string lastMessage = "";
+
+    void Start()
+    {
+        serialManager.serialMessageHandler += messageHandler;
+    }
+
+    void messageHandler(string msg)
+    {
+        messageCount++;
+        lastMessage = msg;
+    }
+
+    [ContextMenu("Start Serial Manager")]
+    void StartSerialManager()
+    {
+        serialManager.Start();
+    }
+
+    [ContextMenu("Stop Serial Manager")]
+    void StopSerialManager()
+    {
+        serialManager.Stop();
+    }
+}
